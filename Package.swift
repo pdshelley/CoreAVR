797b1d7cdfe6b7d2756d07457728a53524a18d50
TheAlgorithm476 <thealgorithm476@gmail.com>
2025-10-27 09:52:07 +0100

    There and back again

--- Package.swift
+++ Package.swift
@@ -12,16 +12,11 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "CCoreAVR",
-            path: "Sources/CCoreAVR",
-            publicHeadersPath: "include"
-        ),
-        .target(
             name: "CoreAVR",
-            dependencies: [
-                .target(name: "CCoreAVR")
-            ],
-            path: "Sources/CoreAVR"
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath(".")
+            ]
         )
     ]
 )
